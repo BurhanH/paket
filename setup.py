d6a26cc4ed95ccba0e82420305d2b8a352e05e2b
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-02-22 13:07:43 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+# -*- coding: utf-8 -*-
 
+from setuptools import setup
+
+
+setup(
+    name='paket',
+    version='0.1',
+    description='simple example to demonstrate python packaging',
+    url='https://github.com/BurhanH/paket',
+    author='Baur Urazalinov',
+    license='MIT',
+    packages=['paket'],
+    zip_safe=False
+)
